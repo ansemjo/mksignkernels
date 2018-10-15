e89754c071d6a39372ceb75d5f17608a0d8c1c1d
Anton Semjonov <anton@semjonov.de>
2018-10-15 21:19:08 +0200

    added pacman hook and updated install and packaging

--- install.mk
+++ install.mk
@@ -1,14 +1,16 @@
 
 DESTDIR :=
 
-BIN := /usr/bin
-ETC := /etc
-DOC := /usr/share/doc
+BIN   := /usr/bin
+ETC   := /etc
+DOC   := /usr/share/doc
+HOOKS := /usr/share/libalpm/hooks
 
 INSTALL := \
 	$(DESTDIR)$(BIN)/mksignkernels \
 	$(DESTDIR)$(ETC)/mksignkernels.mk \
 	$(DESTDIR)$(DOC)/mksignkernels/README.md \
+	$(DESTDIR)$(HOOKS)/96-mksignkernels.hook \
 
 .PHONY: install
 install : $(INSTALL)
@@ -20,4 +22,7 @@ $(DESTDIR)$(ETC)/% : %
 	install -D -b -m 644 -t $(@D) $<
 
 $(DESTDIR)$(DOC)/mksignkernels/% : %
+	install -D -m 644 -t $(@D) $<
+
+$(DESTDIR)$(HOOKS)/% : %
 	install -D -m 644 -t $(@D) $<
